1bea413596d032d37d54c6197cdf038edb730811
Nizar Putra Alghifary <nizarputraalghifary@gmail.com>
2024-11-20 08:46:35 +0700

    Update Donate.tsx

--- components/component/Donate.tsx
+++ components/component/Donate.tsx
@@ -5,7 +5,7 @@ export const Donate = () => {
   return (
     <div className="flex items-center justify-center">
       <a 
-        href="https://saweria.co/nizaralghifary" 
+        href="https://www.nizaralghifary.my.id/supportme" 
         target="_blank" 
         rel="noopener noreferrer"
         className="inline-flex items-center px-4 py-2 bg-yellow-400 text-black font-semibold rounded-lg hover:bg-yellow-500 transition"
