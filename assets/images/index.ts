18e12d1f2ceb44e2222428accd7d29651e48f958
nizaralghifary <nizarputraalghifary@gmail.com>
2025-03-22 08:58:45 +0700

    change meet to neura

--- assets/images/index.ts
+++ assets/images/index.ts
@@ -2,3 +2,5 @@ export { default as mern } from "./mern.svg";
 export { default as terminal } from "./terminal.png";
 export { default as memoirix } from "./memoirix.svg";
 export { default as meet } from "./MeetLogo.png";
+export { default as meet } from "./MeetLogo.png";
+export { default as neura } from "./Neura.svg";
--- constants/index.ts
+++ constants/index.ts
@@ -1,5 +1,6 @@
 import { HTML, CSS, JS, TS, PY, NodeJS, ReactJS, TailwindCSS } from "@/assets/icons/index";
 import { mern, terminal, memoirix, meet } from "@/assets/images/index";
+import { mern, terminal, memoirix, meet, neura } from "@/assets/images/index";
 
 export const skills = [
   {
@@ -73,5 +74,10 @@ export const projects = [
     description: "Aplikasi konferensi video seperti Zoom (Zoom Clone) dibuat dengan Next.js",
     link: "https://meet.nizaralghifary.my.id/",
     download: "/downloads/QuantumMeet.apk",
+  },
+    image: neura,
+    name: "Neura",
+    description: "Platform Generative AI & Image Generator dibuat dengan Next.js",
+    link: "https://neura.nizaralghifary.my.id/",
   },
 ];
